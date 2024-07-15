f14cf873e855d5a58ed18c58257b4befd72e0b28
Mac-324 <mtpc-324@Mac-324s-Mac-mini.local>
2024-07-15 16:50:12 +0530

    backup

--- src/screens/AuthScreens/Login/index.js
+++ src/screens/AuthScreens/Login/index.js
@@ -1,20 +1,29 @@
-import { Alert, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import { moderateScale, scale, verticalScale } from 'react-native-size-matters'
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    KeyboardAvoidingView,
+    TouchableOpacity,
+    Image,
+} from 'react-native';
+import React, { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { moderateScale, scale, verticalScale } from 'react-native-size-matters';
 import firestore from '@react-native-firebase/firestore';
-import NavigationStrings from '../../../constants/NavigationStrings'
-import { Loader } from '../../../components'
-import { setItem } from '../../../AsyncStorage'
+import NavigationStrings from '../../../constants/NavigationStrings';
+import { Loader } from '../../../components';
+import { setItem } from '../../../AsyncStorage';
 import imagePaths from '../../../constants/imagePaths';
 
 const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loaderVisible, setLoaderVisible] = useState(false);
-    const [showLogo, setShowLogo] = useState(true);
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
-    const loginUser = () => {
+    const userDetailCheck = () => {
         setLoaderVisible(true);
         firestore()
             .collection('users')
@@ -24,56 +33,101 @@ const LoginScreen = ({ navigation }) => {
                 setLoaderVisible(false);
                 if (res.docs !== null) {
                     //console.log(res.docs[0].data());
-                    goToNext(res.docs[0].data().userId);
+                    if (password !== res.docs[0].data().password) {
+                        setPasswordError('Incorrect Password');
+                    } else {
+                        goToNext(res.docs[0].data().userId);
+                        setLoaderVisible(false);
+                    }
                 } else {
-                    Alert.alert('User Not Found!');
+                    setEmailError('User Not Found!');
                 }
             })
             .catch(err => {
                 setLoaderVisible(false);
                 console.log(err);
-                Alert.alert('User Not Found!');
+                setEmailError('User Not Found!');
             });
+
     };
-    const goToNext = async (userId) => {
+    const goToNext = async userId => {
         await setItem('USERID', userId);
         navigation.replace(NavigationStrings.APPSTACK);
     };
+
+
+    const validationProcess = () => {
+        if (email === '') {
+            setEmailError('Please enter the email');
+        } else {
+            setEmailError('');
+        }
+
+        if (password === '') {
+            setPasswordError('Please enter the password');
+        } else {
+            setPasswordError('');
+        }
+
+        if (email !== '' && password !== '') {
+            return true;
+        }
+    };
+
+    const loginProcess = () => {
+        if (validationProcess() === true) {
+            userDetailCheck();
+        }
+    }
+
     return (
         <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={styles.main} >
-            <View style={styles.headerContainer}>
-                {showLogo && <Image
-                    source={imagePaths.WHITE_APP_LOGO}
-                    resizeMode='contain'
-                    style={styles.image} />}
+            style={styles.main}>
 
+            <View style={styles.headerContainer}>
+                <Image
+                    source={imagePaths.WHITE_APP_LOGO}
+                    resizeMode="center"
+                    style={styles.image}
+                />
             </View>
-            <View >
-                <View >
+
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'position'}
+                style={{ flex: 3 }}>
+                <View>
                     <View style={styles.Container}>
                         <View style={styles.inputMain}>
-                            <View style={styles.inputView}>
-                                <Ionicons name={'mail-outline'} size={scale(30)} color={'#AAAAAA'} />
-                                <TextInput
-                                    placeholder={'Enter your Email'}
-                                    style={styles.input}
-                                    placeholderTextColor={'#999999'}
-                                    value={email}
-                                    onChangeText={text => setEmail(text)}
-                                    autoCapitalize={false}
-                                    autoComplete={'false'}
-                                    autoFocus={false}
-                                    autoCorrect={false}
-                                    onFocus={() => { setShowLogo(false) }}
-                                    onBlur={() => { setShowLogo(true) }}
-                                />
+                            <View>
+                                <View style={styles.inputView}>
+                                    <Ionicons
+                                        name={'mail-outline'}
+                                        size={scale(30)}
+                                        color={'#AAAAAA'}
+                                    />
+                                    <TextInput
+                                        placeholder={'Enter your Email'}
+                                        style={styles.input}
+                                        placeholderTextColor={'#999999'}
+                                        value={email}
+                                        onChangeText={text => setEmail(text)}
+                                        autoCapitalize={false}
+                                        autoComplete={'false'}
+                                        autoFocus={false}
+                                        autoCorrect={false}
+                                    />
+                                </View>
+                                <Text style={styles.errText}>{emailError ? emailError : null}</Text>
                             </View>
 
                             <View>
                                 <View style={styles.inputView}>
-                                    <Ionicons name={'key-outline'} size={scale(30)} color={'#AAAAAA'} />
+                                    <Ionicons
+                                        name={'key-outline'}
+                                        size={scale(30)}
+                                        color={'#AAAAAA'}
+                                    />
                                     <TextInput
                                         placeholder={'Enter your Password'}
                                         style={styles.input}
@@ -84,49 +138,53 @@ const LoginScreen = ({ navigation }) => {
                                         autoComplete={'none'}
                                         autoFocus={false}
                                         autoCorrect={false}
-                                        onFocus={() => { setShowLogo(false) }}
-                                        onBlur={() => { setShowLogo(true) }}
                                     />
                                 </View>
-                                <Text style={styles.forgotPassword}>Forgot Password?</Text>
+                                <Text style={styles.errText}>{passwordError ? passwordError : null}</Text>
+                                <View
+                                    style={{
+                                        marginLeft: scale(30),
+                                        marginTop: verticalScale(8),
+                                    }}>
+                                    <Text style={styles.forgotPassword}>Forgot Password?</Text>
+                                </View>
                             </View>
                         </View>
+
                         <View style={styles.buttonsView}>
                             <TouchableOpacity
-                                activeOpacity={.9}
+                                activeOpacity={0.9}
                                 style={styles.loginView}
-                                onPress={loginUser}
-                            >
+                                onPress={loginProcess}>
                                 <Text style={styles.loginText}>Login</Text>
                             </TouchableOpacity>
 
                             <View style={styles.registerView}>
                                 <Text style={styles.registerText}>Don't have an account?</Text>
                                 <TouchableOpacity
-                                    activeOpacity={.9}
+                                    activeOpacity={0.9}
                                     onPress={() => {
-                                        navigation.navigate(NavigationStrings.SIGNUP)
+                                        navigation.navigate(NavigationStrings.SIGNUP);
                                     }}>
-
                                     <Text style={styles.registerNavButton}>REGISTER</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
                     </View>
                     <Loader visible={loaderVisible} />
+                </View>
+            </KeyboardAvoidingView>
 
-                </View >
-            </View>
-        </KeyboardAvoidingView >
-    )
-}
+        </KeyboardAvoidingView>
+    );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({
     main: {
         flex: scale(1),
-        backgroundColor: '#FFFFFF'
+        backgroundColor: '#FFFFFF',
     },
     headerContainer: {
         flex: scale(2),
@@ -134,25 +192,25 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: scale(24),
         borderBottomRightRadius: scale(24),
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     image: {
         height: verticalScale(150),
         width: scale(150),
     },
     Container: {
-        bottom: scale(50),
+        bottom: verticalScale(50),
         backgroundColor: '#FFFFFF',
         marginHorizontal: scale(15),
         borderRadius: scale(30),
         alignItems: 'center',
-        shadowColor: "#000",
+        shadowColor: '#000',
         shadowOffset: {
             width: scale(4),
             height: verticalScale(3),
         },
-        shadowOpacity: scale(0.30),
-        shadowRadius: scale(4.30),
+        shadowOpacity: scale(0.3),
+        shadowRadius: scale(4.3),
         elevation: scale(13),
     },
 
@@ -169,16 +227,14 @@ const styles = StyleSheet.create({
         paddingVertical: verticalScale(6),
         backgroundColor: '#EEEEEE',
         fontSize: scale(19),
-        borderRadius: scale(150)
+        borderRadius: scale(150),
     },
     input: {
         padding: moderateScale(0),
         width: scale(250),
-        color: '#000000'
+        color: '#000000',
     },
     forgotPassword: {
-        marginLeft: scale(30),
-        marginTop: verticalScale(8),
         color: '#999999',
         fontWeight: '800',
         fontStyle: 'italic',
@@ -186,8 +242,7 @@ const styles = StyleSheet.create({
     buttonsView: {
         marginBottom: verticalScale(50),
         marginTop: verticalScale(50),
-        gap: verticalScale(8)
-
+        gap: verticalScale(8),
     },
     loginView: {
         backgroundColor: '#ED820B',
@@ -207,10 +262,17 @@ const styles = StyleSheet.create({
         gap: scale(5),
     },
     registerText: {
-        fontWeight: '500'
+        fontWeight: '500',
     },
     registerNavButton: {
         color: '#0000FFAA',
-        fontWeight: '600'
+        fontWeight: '600',
+    },
+    errText: {
+        color: '#FF0000',
+        fontSize: scale(12),
+        fontWeight: '500',
+        marginTop: verticalScale(5),
+        marginLeft: scale(15)
     }
-})
+});
